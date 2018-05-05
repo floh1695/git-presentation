0017d1358daab6bcde585934b0ea7bd306d29155
Bayley <cbayley@proforma.com>
2018-05-05 15:46:14 -0400

    Added more

--- run.bash
+++ run.bash
@@ -1,32 +1,56 @@
 #!/bin/bash
 
-mkdir -p repos
+repos_dir='repos'
 
-pushd repos
+next_commit_number=0
+function next_commit ()
+{
+  filename="${next_commit_number}.txt"
+  next_commit_number=$(($next_commit_number + 1))
+
+  touch $filename
+  git add $filename
+  git commit -m "$filename"
+}
+
+function git_config ()
+{
+  git config user.email 'a@fake.com'
+  git config user.name 'a'
+}
+
+rm -rf $repos_dir
+mkdir -p $repos_dir
+pushd $repos_dir
   
-  rm -rf repo-a
   mkdir -p repo-a
   
   pushd repo-a    
+    echo '--- REPO_A --- START ---'
+    
     git init
-    
-    git config user.email 'a@fake.com'
-    git config user.name 'a'
+    git_config
 
-    git branch apple
-    git branch banana
+    # Initial commit is required before branching
+    next_commit
     
-    git commit --allow-empty-message
+    git branch apple
+    git branch banana  
+    
+    next_commit
     
     git checkout apple
-    git commit --allow-empty-message
-
+    next_commit
+    
     git checkout banana
-    git commit --allow-empty-message
+    next_commit
+    git tag rebase-banana
     git rebase master
-
+    
     git checkout master
-    git merge apple
+    git merge apple --no-edit
+    
+    echo '--- REPO_A --- END ---'
   popd
 popd
 
